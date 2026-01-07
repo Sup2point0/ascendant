425311e8c653353b333fec5b6150263ae303a3ee
Sup#2.0 <sup2point0@gmail.com>
2026-01-07 18:58:39 +0000

    dev - use nested arrays for grid

--- src/puzzle.rs
+++ src/puzzle.rs
@@ -1,11 +1,14 @@
 use std::*;
 
+use itertools::*;
+
 use crate::utils;
 
 
 type Digit = u8;
 
 
+#[derive(Clone, PartialEq, Eq)]
 pub enum Cell {
     Solved(Digit),
     Pencil(collections::HashSet<Digit>),
@@ -45,7 +48,7 @@ impl<const N: usize> Clues<N>
 
 pub struct Grid<const N: usize>
 {
-    cells: Vec<Cell>,
+    cells: [[Cell; N]; N],
     clues: Clues<N>,
 }
 
@@ -55,18 +58,17 @@ impl<const N: usize> Grid<N>
     {
         let mut clues = Clues::new();
 
-        Self {
-            cells:
-                data
-                .into_iter()
+        let cells =
+            data.into_iter()
                 .enumerate()
                 .filter_map(|(y, row)| match y {
                     0              => { Self::prep_clue_row(row, &mut clues.upper); None }
                     _ if y == N - 1 => { Self::prep_clue_row(row, &mut clues.lower); None }
                     _              => { Some( Self::prep_row(y, row, &mut clues) ) }
-                })
-                .flatten()
-                .collect(),
+                });
+
+        Self {
+            cells: utils::as_array(cells),
             clues,
         }
     }
@@ -76,9 +78,7 @@ impl<const N: usize> Grid<N>
         clue_row: &mut [Option<Digit>; N]
     ) -> ()
     {
-        let row = row
-            .into_iter()
-            .enumerate()
+        let row = row.into_iter().enumerate()
             .filter_map(|(x, n)|
                 if x == 0 || x == N - 1 {
                     None
@@ -95,20 +95,12 @@ impl<const N: usize> Grid<N>
         clues: &mut Clues<N>,
     ) -> [Cell; N]
     {
-        let row = row
-            .into_iter()
-            .enumerate()
+        let row = row.into_iter().enumerate()
             .filter_map(|(y, n)|
-                if y == 0 {
-                    clues.left[x - 1] = Some(n); None
-                } else if y == N - 1 {
-                    clues.right[x - 1] = Some(n); None
-                }
-                else if n > 0 {
-                    Some( Cell::Solved(n) )
-                } else {
-                    Some( Cell::Pencil((1..=N as Digit).collect()) )
-                }
+                if      y == 0    { clues.left[x - 1] = Some(n); None }
+                else if y == N - 1 { clues.right[x - 1] = Some(n); None }
+                else if n > 0     { Some(Cell::Solved(n)) }
+                else              { Some(Cell::Pencil((1..=N as Digit).collect())) }
             );
 
         utils::as_array(row)
@@ -117,13 +109,22 @@ impl<const N: usize> Grid<N>
 
 impl<const N: usize> Grid<N>
 {
-    // pub fn cells(&self) -> &[Cell; N.pow()]
-    // {
-    //     self.cells
-    // }
-
-    pub fn rows(&self) -> &[[Cell; N]]
-    {
-        self.cells.as_chunks().0
+    pub fn cells(&self) -> &[[Cell; N]; N] {
+        &self.cells
     }
+
+    pub fn clues(&self) -> &Clues<N> {
+        &self.clues
+    }
+}
+
+impl<const N: usize> Grid<N>
+{
+    pub fn at(&self, x: usize, y: usize) -> &Cell {
+        &self.cells[y][x]
+    }
+
+    // pub fn look_right(&self, row: usize) -> [&Cell; N] {
+    //     self.cells[row]
+    // }
 }
