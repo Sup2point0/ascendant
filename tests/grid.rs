c62abdf9ef05d3ae47cecf9d0ee2641e69c040c0
Sup#2.0 <sup2point0@gmail.com>
2026-01-07 18:58:49 +0000

    dev - add tests

--- tests/grid.rs
+++ tests/grid.rs
@@ -1,18 +1,25 @@
 use ascendant::puzzle::*;
+use ascendant::puzzle::Cell::{Solved as Sv};
 
 
 #[test] pub fn construct()
 {
-    let grid = Grid::construct(
+    let grid: Grid<3> = Grid::construct(
         [
-            [1, 2, 3],
-            [4, 5, 6],
-            [7, 8, 9],
-        ]
+            [0, 0,  0,  0,  0].into(),
+            [0, 10, 20, 30, 0].into(),
+            [0, 40, 50, 60, 0].into(),
+            [0, 70, 80, 90, 0].into(),
+            [0, 0,  0,  0,  0].into(),
+        ].into()
     );
 
     assert_eq!(
-        grid.cells,
-        vec![1..=9]
+        *grid.cells(),
+        [
+            [Sv(10), Sv(20), Sv(30)],
+            [Sv(40), Sv(50), Sv(60)],
+            [Sv(70), Sv(80), Sv(90)],
+        ]
     );
 }
