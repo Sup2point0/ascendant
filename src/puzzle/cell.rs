7d6ec688c7d91c00be72a424d350c4b1f38551eb
Sup#2.0 <sup2point0@gmail.com>
2026-01-07 22:20:37 +0000

    feat - prettify grid output

--- src/puzzle/cell.rs
+++ src/puzzle/cell.rs
@@ -1,28 +1,72 @@
 use std::*;
+use std::collections::HashSet;
 
-use super::*;
+use crate::*;
 
 
 #[derive(Clone, PartialEq, Eq)]
 pub enum Cell {
     Solved(Digit),
-    Pencil(collections::HashSet<Digit>),
+    Pencil(Option<HashSet<Digit>>),
 }
 
 #[macro_export]
 macro_rules! pen {
     ( $($digit: expr),* $(,)? ) =>
     {
-        Cell::Pencil(std::collections::HashSet::from([$( $digit, )*]))
+        Cell::Pencil(
+            Some(
+                std::collections::HashSet::from(
+                    [$( $digit, )*]
+                )
+            )
+        )
+    }
+}
+
+impl Cell
+{
+    pub fn render<const N: usize>(&self) -> String
+    {
+        match self {
+            Self::Solved(d) => {
+                let fill = iter::repeat_n(' ', N+1).collect::<String>();
+
+                format!("{fill}{d}")
+            },
+
+            Self::Pencil(Some(digits)) => {
+                let str = digits.iter()
+                    .map(|n| n.to_string())
+                    .collect::<Vec<String>>()
+                    .join("");
+
+                let c = str.clone().chars().count();
+                let fill = iter::repeat_n(' ', N - c).collect::<String>();
+
+                format!("[{fill}{str}]")
+            },
+
+            Self::Pencil(None) => iter::repeat_n(' ', N+2).collect::<String>(),
+        }
     }
 }
 
 impl fmt::Debug for Cell
 {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result
+    {
         write!(f, "{}", match self {
             Self::Solved(d)  => format!("'{d}'"),
-            Self::Pencil(ds) => format!("[{}]", ds.iter().map(|n| n.to_string()).collect::<Vec<String>>().join("")),
+
+            Self::Pencil(Some(digits)) => format!("[{}]",
+                digits.iter()
+                    .map(|n| n.to_string())
+                    .collect::<Vec<String>>()
+                    .join("")
+            ),
+
+            Self::Pencil(None) => format!("[ ? ]"),
         })
     }
 }
