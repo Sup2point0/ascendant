738f83debcad1c9230a011ea4adc45adff3d937f
Sup#2.0 <sup2point0@gmail.com>
2026-01-15 13:54:24 +0000

    fix - add file name when saving puzzle data

--- src/reader/save.rs
+++ src/reader/save.rs
@@ -11,16 +11,16 @@ pub struct Saver;
 
 impl Saver
 {
-    pub fn save(data: impl serde::Serialize) -> ah::Result<()>
+    pub fn save<const N: usize>(data: impl serde::Serialize) -> ah::Result<()>
     {
         println!(".. saving data...");
         
         let text = json::to_string_pretty(&data)?;
-
         let re = Regex::new("\n {8,10}").unwrap();
         let text = re.replace_all(&text, "");
 
-        fs::write(DATA_ROUTE, text.into_owned())?;
+        let route = format!("{DATA_ROUTE}/{N}x{N}-puzzles.json");
+        fs::write(route, text.into_owned())?;
 
         Ok(())
     }
--- src/runner/fetch.rs
+++ src/runner/fetch.rs
@@ -16,18 +16,18 @@ pub async fn fetch_load_save<const N: usize>(difficulty: Difficulty) -> ah::Resu
     let mut grids_fetched = Fetcher::fetch::<N>(urls).await?;
 
     let mut grids = Loader::load_grids::<N>()?;
-    
-    grids.get_mut(&difficulty.to_string())
-        .unwrap_or(&mut vec![])
+
+    grids.entry(difficulty.to_string())
+        .or_insert(vec![])
         .append(&mut grids_fetched);
 
     let grids_data = grids.into_iter().
-        map(|(size, grids)|
-            (size, grids.into_iter().map(GridExchange::from).collect_vec())
+        map(|(diff, grids)|
+            (diff, grids.into_iter().map(GridExchange::from).collect_vec())
         )
         .collect::<HashMap<String, Vec<GridExchange>>>();
 
-    Saver::save(grids_data)?;
+    Saver::save::<N>(grids_data)?;
 
     Ok(())
 }
